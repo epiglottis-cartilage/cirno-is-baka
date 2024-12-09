962d9aaa80fd22326eda796d4cf9822b861893cb
epiglottis <epiglottis.cartilage@gmail.com>
2024-12-09 17:30:04 +0800

    paly audio when start

--- build.rs
+++ build.rs
@@ -6,7 +6,7 @@ fn main() {
     // https://learn.microsoft.com/zh-cn/windows/apps/design/style/iconography/app-icon-construction
     let mut res = winres::WindowsResource::new();
     res.set_icon("src/cirno.ico");
-    res.set("LegalCopyright", "Application created by Arrokoth233");
+    res.set("LegalCopyright", "Application created by epiglottis, forked from Arrokoth233");
     res.set_language(0x0000);
     res.compile().unwrap();
     // Build::new().compile("icon/resources.rc").unwrap();
